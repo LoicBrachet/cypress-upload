a30e6eaff41378b763ba8acd5bc7e8f380287fcc
auberon33 <51779120+auberon33@users.noreply.github.com>
2022-04-11 11:46:06 +0200

    test up/down

---
+++ cypress/integration/downloadfile.spec.js
@@ -0,0 +1,31 @@
+describe('File upload and download tests', () => {
+
+    beforeEach(() => {
+        cy.visit('https://filebin.net/')
+    })
+
+    it('Upload file and download it in Zip format', () => {
+        cy.get('#fileField').attachFile('fileToUpload.jpg')
+        cy.contains('was created',).should('be.visible')
+        cy.get('[data-bs-target="#modalArchive"]').click({force:true})
+        cy.contains('Zip').invoke('attr', 'href').then(downloadLink => {
+            cy.log(downloadLink)
+            let dl="https://filebin.net/"+downloadLink
+            cy.downloadFile(dl, 'mydownloads/zipFiles', 'downloadedFromCypress.zip')
+            cy.readFile('mydownloads/zipFiles/downloadedFromCypress.zip')
+        })
+    })
+
+    it('Upload file and download it in Tar format', () => {
+        cy.get('#fileField').attachFile('fileToUpload.jpg')
+        cy.contains('was created',).should('be.visible')
+        cy.get('[data-bs-target="#modalArchive"]').click()
+        cy.contains('Tar').invoke('attr', 'href').then(downloadLink => {
+            cy.log(downloadLink)
+            let dl="https://filebin.net/"+downloadLink
+            cy.downloadFile(dl, 'mydownloads/tarFiles', 'downloadedFromCypress.tar')
+            cy.readFile('mydownloads/tarFiles/downloadedFromCypress.tar')
+        })
+    })
+
+})
